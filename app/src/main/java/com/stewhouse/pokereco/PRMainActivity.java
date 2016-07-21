a91fb61dd20f41aa945a5d606b73b75e31a41f53
Alphys <je.park@memebox.com>
2016-07-21 16:18:33 +0900

    1. Add manifest options. 2. Add drawer UI.

--- app/src/main/java/com/stewhouse/pokereco/PRMainActivity.java
+++ app/src/main/java/com/stewhouse/pokereco/PRMainActivity.java
@@ -1,14 +1,84 @@
 package com.stewhouse.pokereco;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.Fragment;
+import android.app.FragmentManager;
 import android.os.Bundle;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.AppCompatActivity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.BaseAdapter;
+import android.widget.ListView;
 
 public class PRMainActivity extends AppCompatActivity {
+
+    private DrawerLayout mDrawerLayout = null;
+    private ListView mDrawerList = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.activity_main);
+
+        initDrawer();
+    }
+
+    private void initDrawer() {
+        mDrawerList = (ListView) findViewById(R.id.drawer_list);
+        mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+
+        if (mDrawerList != null) {
+            mDrawerList.setAdapter(new PRDrawerAdapter());
+            mDrawerList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+
+                @Override
+                public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+
+                    if (position == 0) {    // IV calculator Cell.
+                        Fragment fragment = new PRIVCalculatorFragment();
+                        FragmentManager fragmentManager = getFragmentManager();
+
+                        fragmentManager.beginTransaction().replace(R.id.content_frame, fragment).commit();
+                    }
+
+                    mDrawerLayout.closeDrawers();
+                }
+            });
+        }
+    }
+
+    private class PRDrawerAdapter extends BaseAdapter {
+
+        @Override
+        public int getCount() {
+            return 1;
+        }
+
+        @Override
+        public Object getItem(int position) {
+            return null;
+        }
+
+        @Override
+        public long getItemId(int position) {
+            return position;
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            if (convertView == null) {
+                LayoutInflater layoutInflater = LayoutInflater.from(PRMainActivity.this);
+
+                // IV calculator Cell Setting.
+                if (position == 0) {
+                    convertView = layoutInflater.inflate(R.layout.cell_drawer_iv, parent, false);
+                }
+            }
+
+            return convertView;
+        }
     }
 }
---
+++ app/src/main/java/com/stewhouse/pokereco/PRMainFragment.java
@@ -0,0 +1,20 @@
+package com.stewhouse.pokereco;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by Gomguk on 16. 7. 21..
+ */
+public class PRMainFragment extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_main, container, false);
+    }
+}
