0ba15b14b7dca7bcd10549a5921b1908bd1c8aee
Alphys <je.park@memebox.com>
2016-07-21 14:28:37 +0900

    Initial commit.

---
+++ app/src/main/java/com/stewhouse/pokereco/pokereco/PMainActivity.java
@@ -0,0 +1,14 @@
+package com.stewhouse.pokereco.pokereco;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class PMainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_main);
+    }
+}
